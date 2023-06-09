6ddad568b35023165738fc7e023db3c45a62d6cd
Artemis <132653903+Artemis-IA@users.noreply.github.com>
2023-06-09 12:23:49 +0200

    Add files via upload

---
+++ data/data.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import pandas as pd
+import streamlit as st
+
+def format_heure(dataframe):
+    dataframe['heure'] = pd.to_timedelta(dataframe['heure']).apply(lambda x: f"{x.components.hours}:{x.components.minutes:02d}:{x.components.seconds:02d}")
+    return dataframe
+
+@st.cache_data
+def get_data(query):
+    # Connection à la BDD
+    mydb = mysql.connector.connect(host="localhost", port="3307", user="client", password="example", database="breizhibus")
+    # Le curseur permet d'intéragir avec la BDD
+    db=mydb.cursor()
+
+    db.execute(query)
+    dataframe = pd.DataFrame(db.fetchall(), columns = [i[0] for i in db.description])
+
+    db.close()
+    mydb.close()
+
+    return dataframe
+
+def style():
+    st.set_page_config(
+    page_title="Breizhibus",
+    page_icon="🚌",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
